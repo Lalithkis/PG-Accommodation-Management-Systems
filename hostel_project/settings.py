e28894642049a82fe6e655328b5f1e082ea101b2
Lalith <lalithkishore325@gmail.com>
2026-02-15 21:08:57 +0530

    Force ALLOWED_HOSTS to wildcard

--- hostel_project/settings.py
+++ hostel_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-l-^66kx*m6dao2++-cl48
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
