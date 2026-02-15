8e8ff6664e6592b44872bed492d6418cf5e5718d
Lalith <lalithkishore325@gmail.com>
2026-02-15 19:16:41 +0530

    Fix Vercel build: remove manual pip install

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,1 @@
-python3 -m ensurepip
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
