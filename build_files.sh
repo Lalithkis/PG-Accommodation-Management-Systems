e81fb62789c407b50df3dd8cc67cd5bc81f28971
Lalith <lalithkishore325@gmail.com>
2026-02-15 19:00:23 +0530

    Fix Vercel build script and runtime

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
